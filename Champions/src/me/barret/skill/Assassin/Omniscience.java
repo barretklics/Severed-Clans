7df10b259115382b0efbcf68d2849f94ccb6f37e
ethvor <118936291+ethvor@users.noreply.github.com>
2023-01-15 03:15:22 -0500

    glow v1 - adds glow locally for user to all players on server except self, removes after 2 + lvl seconds. No proximity yet. Has weird bug where it removes glow if the glwoing player sprint status changes state

--- Champions/src/me/barret/skill/Assassin/Omniscience.java
+++ Champions/src/me/barret/skill/Assassin/Omniscience.java
@@ -1,26 +1,35 @@
 package me.barret.skill.Assassin;
 import me.barret.Champions;
+import me.barret.events.TickUpdateEvent;
 import me.barret.kits.Kit;
 import me.barret.kits.kitManager;
 import me.barret.skill.Skill;
 import me.barret.skill.interactSkill;
-import me.barret.utils.addLocalGlow;
+import me.barret.utils.localGlow;
 import me.barret.user.user;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+
+import java.util.HashMap;
 
 public class Omniscience extends Skill implements interactSkill {
 
-    private static Kit skillKit = kitManager.getKitFromString("Assassin"); 		//sets class for skill to assassin
+    private static HashMap<Player, Long> timeOfGlow = new HashMap<Player, Long>();
 
-    private static SkillType skillType = SkillType.SWORD; 						//sets skill type to sword skill
+    private static HashMap<Player, Boolean> glowActivated = new HashMap<Player, Boolean>();
+    private static HashMap<Player, Integer> playerLevel = new HashMap<Player, Integer>();
+
+    private static Kit skillKit = kitManager.getKitFromString("Assassin");        //sets class for skill to assassin
+
+    private static SkillType skillType = SkillType.SWORD;                        //sets skill type to sword skill
 
     static String skillName = "Omniscience";
 
     static String[] description = {"See all. Know all."};
 
     static int MaxLevel = 3;
-    public Omniscience(Champions i)
-    {
+
+    public Omniscience(Champions i) {
         super(i, skillKit, skillName, skillType, description, MaxLevel);
 
     }
@@ -30,13 +39,31 @@ public class Omniscience extends Skill implements interactSkill {
     public void activate(Player p, user u, int lvl)  //activate occurs on right click - lvl is ability level selected in ench table
     {
         p.sendMessage("Activated Skill");
-        doOmniscience(p,lvl);
+        playerLevel.put(p, lvl);
+        doOmniscience(p, lvl);
     }
 
-    void doOmniscience(Player p, int lvl)
-    {
+    public void doOmniscience(Player p, int lvl) {
+
         p.sendMessage("trying to add glow");
-        addLocalGlow.addGlow(p);
+        localGlow.addGlow(p, lvl);
+        timeOfGlow.put(p, System.currentTimeMillis());
+        glowActivated.put(p, true);
         p.sendMessage("post glow line in code");
+
+
+    }
+
+    @EventHandler
+    public void OnTickUpdate(TickUpdateEvent e) {
+        for (Player p : timeOfGlow.keySet()) {
+            if (glowActivated.get(p)) {
+                if (System.currentTimeMillis() >= timeOfGlow.get(p) + 2000 + 1000 * playerLevel.get(p)) {
+
+                    localGlow.removeLocalGlow(p, playerLevel.get(p));
+                    glowActivated.put(p, false);
+                }
+            }
+        }
     }
 }
---
+++ Core/src/me/barret/utils/localGlow.java
@@ -0,0 +1,138 @@
+package me.barret.utils;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.WrappedDataValue;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+import com.comphenix.protocol.wrappers.WrappedWatchableObject;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+
+
+//Entity Effect
+//Packet ID	- State	- Bound To - Field Name	- Field Type - Notes
+//  0x68	  Play	  Client	  Entity ID	   VarInt	    x
+//                                 Effect ID	VarInt	  (look up ids for pot effects)
+//                                 Amplifier    Byte	   Notchian client displays effect level as Amplifier + 1.
+//                                 Duration 	VarInt	   Duration in ticks.
+//                                  Flags	    Byte	    Bit field, see below.
+//                             Has Factor Data - Boolean -  Used in DARKNESS effect
+//                              Factor Codec -   NBT Tag -   See below
+
+
+
+public class localGlow {
+
+    private static HashMap<Player, Player> glowingPlayersForAbilityUser = new HashMap<Player, Player>();
+
+    public static void addGlow(Player player, int lvl) {
+
+        //timeOfGlow.put(player, System.currentTimeMillis()); //used to remove glow from players after certain time
+
+
+
+            for (Player glowReceiver : Bukkit.getOnlinePlayers()) {
+                if (glowReceiver != player) {
+                    glowingPlayersForAbilityUser.put(player, glowReceiver);
+
+                    player.sendMessage("within addGlow debug");
+
+
+                    ProtocolManager Manager = ProtocolLibrary.getProtocolManager();
+
+                    PacketContainer packet = Manager.createPacket(PacketType.Play.Server.ENTITY_METADATA);
+
+                    packet.getIntegers().write(0, glowReceiver.getEntityId()); //Set packet's entity id                 //not sure whether this is the person who sees glow or person who gets glow.
+
+                    WrappedDataWatcher dataWatcher = new WrappedDataWatcher(); //Create data watcher, the Entity Metadata packet requires this
+
+                    WrappedDataWatcher.Serializer serializer = WrappedDataWatcher.Registry.get(Byte.class); //Found this through google, needed for some stupid reason
+
+                    dataWatcher.setEntity(player); //Set the new data watcher's target
+
+                    dataWatcher.setObject(0, serializer, (byte) (0x40)); //Set status to glowing, found on protocol page
+
+
+                    final List<WrappedDataValue> wrappedDataValueList = new ArrayList<>();
+
+                    for (final WrappedWatchableObject entry : dataWatcher.getWatchableObjects()) {
+                        if (entry == null) continue;
+
+                        final WrappedDataWatcher.WrappedDataWatcherObject watcherObject = entry.getWatcherObject();
+                        wrappedDataValueList.add(
+                                new WrappedDataValue(
+                                        watcherObject.getIndex(),
+                                        watcherObject.getSerializer(),
+                                        entry.getRawValue()
+                                )
+                        );
+                    }
+
+                    packet.getDataValueCollectionModifier().write(0, wrappedDataValueList);
+
+
+                    Manager.sendServerPacket(player, packet);
+                }
+            }
+
+    }
+
+public static void removeLocalGlow(Player player, int lvl)
+{
+  //  if(timeOfGlow.get(player) + 2000 + lvl <= System.currentTimeMillis())
+        player.sendMessage("attempting glow remove");
+
+    for (Player glowReceiver : Bukkit.getOnlinePlayers()) {
+
+
+            player.sendMessage("within removeGlow debug");
+
+
+            ProtocolManager Manager = ProtocolLibrary.getProtocolManager();
+
+            PacketContainer packet = Manager.createPacket(PacketType.Play.Server.ENTITY_METADATA);
+
+            packet.getIntegers().write(0, glowReceiver.getEntityId()); //Set packet's entity id                 //not sure whether this is the person who sees glow or person who gets glow.
+
+            WrappedDataWatcher dataWatcher = new WrappedDataWatcher(); //Create data watcher, the Entity Metadata packet requires this
+
+            WrappedDataWatcher.Serializer serializer = WrappedDataWatcher.Registry.get(Byte.class); //Found this through google, needed for some stupid reason
+
+            dataWatcher.setEntity(player); //Set the new data watcher's target
+
+            dataWatcher.setObject(0, serializer, (byte) (0)); //REMOVES GLOW EFFECT
+
+
+            final List<WrappedDataValue> wrappedDataValueList = new ArrayList<>();
+
+            for (final WrappedWatchableObject entry : dataWatcher.getWatchableObjects()) {
+                if (entry == null) continue;
+
+                final WrappedDataWatcher.WrappedDataWatcherObject watcherObject = entry.getWatcherObject();
+                wrappedDataValueList.add(
+                        new WrappedDataValue(
+                                watcherObject.getIndex(),
+                                watcherObject.getSerializer(),
+                                entry.getRawValue()
+                        )
+                );
+            }
+
+            packet.getDataValueCollectionModifier().write(0, wrappedDataValueList);
+
+
+            Manager.sendServerPacket(player, packet);
+
+
+        }
+
+    }
+}
+
+
+
